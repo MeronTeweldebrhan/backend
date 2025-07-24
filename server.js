c91f2c2c9ad92fa19e34843b399a28b6a5e0774e
MeronTeweldebrhan <meronqelati@gmail.com>
2025-07-24 11:31:40 -0400

    update cors link

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT =process.env.PORT || 3000;
 
 
 const app = express(); 
-app.use(cors({origin: 'https://your-netlify-site.netlify.app',
+app.use(cors({origin: 'https://fullstackmerno.netlify.app/',
   credentials: true}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
